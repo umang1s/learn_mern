39f4dc010a353527b6b2d26f341bc5163a664e89
=umang <=umang1maurya@gmail.com>
2022-12-14 22:38:27 +0530

    added database connection

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const {errorHandler} = require("./middleware/errorHandler")
 const coockieParser= require("cookie-parser")
 const cors=require('cors')
 const corsOptions=require("./config/corsOptions")
+const connectDB=require('./config/dbConn')
+const mongoose=require('mongoose')
+const {logEvents} = require('./middleware/logger')
 const PORT= process.env.PORT || 8090;
 
 console.log(process.env.NODE_ENV)
+
+connectDB()
 app.use(logger);
 app.use(coockieParser())
 app.use(express.json())
@@ -31,6 +36,15 @@ app.all("*",(req,res)=>{
 })
 app.use(errorHandler);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
+mongoose.connection.once('open',()=>{
+    console.log('Connected to DB')
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`)
+    })
 })
+
+mongoose.connection.on('error', err=>{
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,'mongoErrLog.log')
+})
+
